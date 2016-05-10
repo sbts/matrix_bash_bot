76cd8f5df03b99b82f1a700b91b345f3e320903b
sbts <info@sbts.com.au>
2016-05-11 05:34:24 +0800

    add mpddj_player.sh

---
+++ mpddj_player.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source matrix-bashbot.sh
+
+clear;
+
+cat <<-EOF
+	This script prompts you for a song URL or search string to play
+	I then submits your entry to https://half-shot.uk/stream via
+	matrix room !mpd:halfshot.uk
+	
+	enter
+	    quit: to exit
+	    next: to skip the current song
+	    skip: to skip the current song
+	
+	
+	Song:
+
+EOF
+
+while true; do
+    tput cup 10 6
+    tput el
+    read Search
+    tput ed
+    echo
+    case ${Search:-unknown} in
+        'quit') echo -e "\n\n";
+                exit;;
+        'next') ./matrix-bashbot.sh mpddj_skip;;
+        'skip') ./matrix-bashbot.sh mpddj_next;;
+             *) ./matrix-bashbot.sh mpddj_QueueSong "$Search";;
+    esac
+done
+
+
