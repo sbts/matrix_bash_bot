ee302285e2c6aa729403c3d64e4c8147bd8f3707
sbts <info@sbts.com.au>
2016-05-12 16:49:45 +0800

    add playlist store capabilities

--- mpddj_player.sh
+++ mpddj_player.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 source matrix-bashbot.sh
+mkdir -p ~/tmp
+
+clear;
+
+cat <<-EOF
+	We update 2 playlists as you enter songs.
+	    ~/tmp/mpddj-all.playlist
+	    ~/tmp/mpddj-\$playlist.playlist
+	where you are about to enter a value for \$playlist
+	
+	EOF
+read -e -i 'default' -p 'Please enter a playlist name: '
+touch ~/tmp/mpddj-${playlist:=default}.playlist
 
 clear;
 
@@ -22,7 +35,7 @@ EOF
 while true; do
     tput cup 10 6
     tput ed
-    read Search
+    read -e Search
     tput ed
     echo
     case ${Search:-unknown} in
@@ -33,6 +46,8 @@ while true; do
              *) ./matrix-bashbot.sh mpddj_QueueSong "$Search";;
     esac
     echo
+    echo "$Search" >> ~/tmp/mpddj-$playlist.playlist
+    echo "$Search" >> ~/tmp/mpddj-all.playlist
     for i in {10..1}; do echo -en "\r  $i   "; sleep 1; done
 done
 
